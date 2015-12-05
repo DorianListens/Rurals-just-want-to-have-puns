4d5226d790f50c369082e31b8c9c51543d86aee5
Dorian <dorian.scheidt@gmail.com>
2015-12-05 13:32:22 -0800

    Use commandline args to make request for specific word

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use rustc_serialize::json::{self, Json};
 extern crate curl;
 use curl::http;
 use std::str;
+use std::env;
 
 fn main() {
+    let word = env::args().nth(1).unwrap_or("heart".to_string());
+    println!("the word is really: {}", word);
+    let url = format!("http://rhymebrain.com/talk?function=getRhymes&word={}", word);
     let resp = http::handle()
-        .get("http://rhymebrain.com/talk?function=getRhymes&word=heart")
+        .get(url)
         .exec().unwrap();
 
     let data = str::from_utf8(resp.get_body()).unwrap();
@@ -19,12 +23,15 @@ fn main() {
 
     let mut rhyme_list : Vec<Rhyme> = Vec::new();
     for item in arr {
-        // println!("item: {}", item.to_string());
         let decoded : Rhyme = json::decode(&item.to_string()).unwrap();
-        println!("Look ma, I decoded a {}", decoded.word);
+        // println!("Look ma, I decoded a {}", decoded.word);
         rhyme_list.push(decoded);
     }
 
     rhyme_list.retain( |x| x.score == 300);
     println!("The filtered rhyme list: {}", rhyme_list.len());
+
+    for item in rhyme_list {
+        println!("the word is: {}", item.word);
+    }
 }
